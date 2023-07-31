4e99e4034be487d119a9afcc14762be5ef1d6f0c
nagachen <birdann99@gmail.com>
2023-07-31 22:57:47 +0800

    完成最新文章的顯示與刪除功能

--- view/backend/news.php
+++ view/backend/news.php
@@ -1,5 +1,5 @@
 <form action="./api/news_admin.php" method="post">
-    <table>
+    <table class="ct" style="width:75%;margin:auto">
         <tr>
             <th>編號</th>
             <th>標題</th>
@@ -20,10 +20,15 @@
 
             <td><input type="checkbox" name="del[]" value="<?=$row['id'];?>" 
             ></td>
+            <input type="hidden" name="id[]" value="<?=$row['id'];?>">
             </tr>    
     <?php
         }
     ?>
     </table>
     <div class="ct"><?=$links;?></div>
+
+    <div class="ct">
+    <input type="submit" value="確定修改">
+</div>
 </form>
