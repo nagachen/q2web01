f9b5653a2b3492cf451b7d34c847f9360bcf3e4b
nagachen <birdann99@gmail.com>
2023-08-01 15:47:25 +0800

    完成投票功能

--- controller/Que.php
+++ controller/Que.php
@@ -10,4 +10,10 @@
     function backend(){
         $this->view("./view/backend/que.php");
     }
+    function subject($id){
+        $subject=$this->find($id);
+        $options=$this->all(['subject_id'=>$id]);
+        $subject['options']=$options;
+        return $subject;
+    }
     }
