e07612ebc0ea5e1d75b4d1a23a64215ddd19c0cb
nagachen <birdann99@gmail.com>
2023-08-01 11:55:47 +0800

    撰寫取得文章列表程式

--- view/front/po.php
+++ view/front/po.php
@@ -4,18 +4,30 @@
 <div style="display:flex">
     <fieldset>
         <legend>分類網誌</legend>
-        <div><a class="cat" href='#'>健康新知  </a></div>
-        <div><a class="cat" href='#'>菸害防治</a></div>
-        <div><a class="cat" href='#'>癌症防治</a></div>
-        <div><a class="cat" href='#'>慢性病防治</a></div>
+        <div><a class="cat" data-type='1' href='#'>健康新知  </a></div>
+        <div><a class="cat" data-type='2' href='#'>菸害防治</a></div>
+        <div><a class="cat" data-type='3' href='#'>癌症防治</a></div>
+        <div><a class="cat" data-type='4' href='#'>慢性病防治</a></div>
 
     </fieldset>
     <fieldset style="width:550px;">
         <legend>文章列表</legend>
+        <div id="lists"></div>
+        <div id="post"></div>
+
     </fieldset>
 </div>
 <script>
+    getList(1);
     $(".cat").click(function(){
         $("#header").text($(this).text());
+        let type=$(this).data('type');
+        getList(type);
     })
+
+    function getList(type){
+        $.get('./api/get_list.php',{type},(list)=>{
+            $("#lists").html(list);
+        })
+    }
 </script>
